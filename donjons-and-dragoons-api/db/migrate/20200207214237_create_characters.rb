3821fa9f833d039cc3a6c8ccbb22c94ec630bedc
jesselsmith <jesse.lee.smith@gmail.com>
2020-02-07 13:46:54 -0800

    adding models

---
+++ donjons-and-dragoons-api/db/migrate/20200207214237_create_characters.rb
@@ -0,0 +1,15 @@
+class CreateCharacters < ActiveRecord::Migration[6.0]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :level
+      t.integer :current_hp
+      t.string :armor
+      t.string :weapon
+      t.integer :xp
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
