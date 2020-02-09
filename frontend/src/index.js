e596ad36a387c54fbf6e6f95db701a3cb2d696bc
jesselsmith <jesse.lee.smith@gmail.com>
2020-02-09 13:09:10 -0800

    Fixing some css, adding in buttons for log in and sign up

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+const BASE_URL = "http://localhost:3000"
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("login-btn").addEventListener("click", e =>{
+    console.log("Log In Click")
+  })
+  document.getElementById("signup=btn").addEventListener("click", e =>{
+    console.log("Sign Up Click")
+  })
+})
+
