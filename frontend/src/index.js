2a6c1338b7c011886ba8b66bdee5bf00d33e027b
Jesse <jesse.lee.smith@gmail.com>
2020-02-10 13:21:57 -0800

    adding fetch functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,26 @@ const BASE_URL = "http://localhost:3000"
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("login-btn").addEventListener("click", e => {
+  document.getElementById("login-btn").addEventListener("click", () => {
     document.getElementById("signup-div").classList.add("hidden")
     document.getElementById("login-div").classList.remove("hidden")
   })
-  document.getElementById("signup-btn").addEventListener("click", e => {
+  document.getElementById("signup-btn").addEventListener("click", () => {
     document.getElementById("login-div").classList.add("hidden")
     document.getElementById("signup-div").classList.remove("hidden")
   })
+
+  doc
 })
 
+async function fetchPoster(url, body) {
+  const resp = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(body)
+  })
+  return await resp.json()
+}
