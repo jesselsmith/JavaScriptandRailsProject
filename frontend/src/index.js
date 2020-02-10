6c74171dba40617dc0d659da6726ab5d9ab8994b
Jesse <jesse.lee.smith@gmail.com>
2020-02-10 12:17:12 -0800

    Working on adding login and sign up with hideable forms

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ const BASE_URL = "http://localhost:3000"
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("login-btn").addEventListener("click", e =>{
-    console.log("Log In Click")
+  document.getElementById("login-btn").addEventListener("click", e => {
+    document.getElementById("signup-div").classList.add("hidden")
+    document.getElementById("login-div").classList.remove("hidden")
   })
-  document.getElementById("signup=btn").addEventListener("click", e =>{
-    console.log("Sign Up Click")
+  document.getElementById("signup-btn").addEventListener("click", e => {
+    document.getElementById("login-div").classList.add("hidden")
+    document.getElementById("signup-div").classList.remove("hidden")
   })
 })
 
