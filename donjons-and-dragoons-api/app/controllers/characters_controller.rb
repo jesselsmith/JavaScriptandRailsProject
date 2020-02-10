ee79eca85d9268cc9f07247abe14fa5c1766d6b5
Jesse <jesse.lee.smith@gmail.com>
2020-02-10 10:56:29 -0800

    Made preliminary character controller

--- donjons-and-dragoons-api/app/controllers/characters_controller.rb
+++ donjons-and-dragoons-api/app/controllers/characters_controller.rb
@@ -1,9 +1,50 @@
 class CharactersController < ApplicationController
+  def create
+    character = Character.new(new_character_params(params))
+    character.level = 1
+    character.armor = "chainmail"
+    character.weapon = "longsword"
+    character.current_hp = character.max_hp
+    charcter.xp = 0
+    character.user_id = current_user
+    if character.save
+      render json: character
+    else
+      render json: { message: "There was an error in character creation" }
+    end
+  end
 
+  def destroy
+    character = find_character
+    if character
+      character.destroy
+      render json: character
+    else
+      render json: { message: "That character could not be found" }
+    end
+  end
+
+  def update
+    character = find_character
+    if character
+      character.update(update_character_params(params))
+      render json: character
+    else
+      render json: { message: "That character could not be found" }
+    end
+  end
 
   private
-  
-  def character_params(params)
+
+  def new_character_params(params)
     params.require(:character).permit(:name)
+  end
+
+  def update_character_params(params)
+    params.require(:character).permit(:name, :level, :armor, :weapon, :current_hp, :xp)
+  end
+
+  def find_character
+    Character.find_by(id: params[:id])
   end
 end
