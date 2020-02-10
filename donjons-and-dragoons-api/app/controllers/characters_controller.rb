b4a3e8c757d3caf352621bf6e80b3134bd26abf3
Jesse <jesse.lee.smith@gmail.com>
2020-02-10 10:38:41 -0800

    Working on character model and controller

--- donjons-and-dragoons-api/app/controllers/characters_controller.rb
+++ donjons-and-dragoons-api/app/controllers/characters_controller.rb
@@ -1,2 +1,9 @@
 class CharactersController < ApplicationController
+
+
+  private
+  
+  def character_params(params)
+    params.require(:character).permit(:name)
+  end
 end
--- donjons-and-dragoons-api/app/models/character.rb
+++ donjons-and-dragoons-api/app/models/character.rb
@@ -1,3 +1,7 @@
 class Character < ApplicationRecord
   belongs_to :user
+
+  def max_hp()
+    @level * 8 + 4
+  end
 end
