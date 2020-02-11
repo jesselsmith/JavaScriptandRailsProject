9e57ed726e51a27e0479b7b46f7f0312ee0d53fe
Jesse <jesse.lee.smith@gmail.com>
2020-02-10 16:14:32 -0800

    Working on getting signing up/logging in and out functional

--- donjons-and-dragoons-api/config/initializers/devise.rb
+++ donjons-and-dragoons-api/config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'jesse.lee.smith@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -40,7 +40,7 @@ Devise.setup do |config|
   # session. If you need permissions, you should implement that in a before filter.
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
-  # config.authentication_keys = [:email]
+  config.authentication_keys = [:username]
 
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
@@ -251,7 +251,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  # config.navigational_formats = ['*/*', :html]
+  config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,11 +14,30 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("login-form").addEventListener("submit", e => {
     e.preventDefault()
     loginInfo = {
-      "user[username]": e.target.querySelector('[name="user[username]"').value,
-      "user[password]": e.target.querySelector('[name="user[password]"').value
+      user: {
+        "username": e.target.querySelector('[name="user[username]"').value,
+        "password": e.target.querySelector('[name="user[password]"').value
+      }
     }
-
+    fetchPoster(BASE_URL + "/users/sign_in", loginInfo)
+      .then(processLogin)
+    e.target.reset()
   })
+
+  document.getElementById("signup-form").addEventListener("submit", e => {
+    e.preventDefault()
+    signupInfo = {
+      "user": {
+        "email": e.target.querySelector("#user_email").value,
+        "username": e.target.querySelector("#user_username").value,
+        "password": e.target.querySelector("#user_password").value,
+        "password_confirmation": e.target.querySelector("#user_password_confirmation").value
+      }
+    }
+    fetchPoster(BASE_URL + "/users", signupInfo)
+      .then(processLogin)
+    e.reset()
+  }, false)
 })
 
 async function fetchPoster(url, body) {
@@ -31,4 +50,16 @@ async function fetchPoster(url, body) {
     body: JSON.stringify(body)
   })
   return await resp.json()
+}
+
+function processLogin(json) {
+  if (json.username) {
+    document.getElementById("welcome-text").textContent = `Welcome, ${json.username}!`
+    //hide login and sign up buttons and forms while logged in
+    document.getElementById("new-session-btns").classList.add("hidden")
+    document.getElementById("signup-div").classList.add("hidden")
+    document.getElementById("login-div").classList.add("hidden")
+    //show text welcoming user and logout button while logged in
+    document.getElementById('logged-in-span').classList.remove("hidden")
+  }
 }
