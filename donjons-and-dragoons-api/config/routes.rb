dd59c009e6662f2db02c62faedd9dca1fc09865a
Jesse <jesse.lee.smith@gmail.com>
2020-02-12 17:37:31 -0800

    finalizing set up for jwt, removing some fields from forms for initial testing :

--- donjons-and-dragoons-api/config/routes.rb
+++ donjons-and-dragoons-api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,  controllers: { registrations: 'registrations' }
   resources :monsters
   resources :characters, only: [:index, :show, :create, :update, :destroy]
   root 'home#index'
--- donjons-and-dragoons-api/db/schema.rb
+++ donjons-and-dragoons-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_215222) do
+ActiveRecord::Schema.define(version: 2020_02_13_011217) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2020_02_07_215222) do
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
+  create_table "jwt_blacklist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_blacklist_on_jti"
+  end
+
   create_table "monsters", force: :cascade do |t|
     t.string "name"
     t.string "type"
@@ -36,6 +42,18 @@ ActiveRecord::Schema.define(version: 2020_02_07_215222) do
     t.string "weapon"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "characters", "users"
