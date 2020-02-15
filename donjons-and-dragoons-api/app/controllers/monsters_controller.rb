534ef404da0d07aedffcd36841dceabd5c12efbd
Jesse <jesse.lee.smith@gmail.com>
2020-02-15 00:52:58 -0800

    starting on figuring out monsters controller

--- donjons-and-dragoons-api/app/controllers/monsters_controller.rb
+++ donjons-and-dragoons-api/app/controllers/monsters_controller.rb
@@ -1,2 +1,12 @@
 class MonstersController < ApplicationController
+  respond_to :json
+  def create
+
+  end
+end
+
+private
+
+def create_monster_params(params)
+  params.require(:monster).permit(:name, :level, :armor, :weapon, :current_hp, :xp)
 end
