05686a06622c169f412ee1a2b80bd8a41ec60c13
Jesse <jesse.lee.smith@gmail.com>
2020-02-14 23:38:00 -0800

    started to add an active monster class to the js that would create a monster from an external api source and then persist it to the database, but I realized that was better served inside the actual monster model, and I could call the special constructor in the monster controller, which would send the appropriate monster back to the frontend through the api. Beginning to implement

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -347,11 +347,22 @@ class ActiveCharacter {
   }
 
   attack(monster, advantage = 'straight') {
-    const attackRoll = this.attackBonus + this.advantageRoll(advantage)
-    if (attackRoll >= monster.armorClass) {
+    const dieRoll = this.advantageRoll(advantage)
+    const attackRoll = this.attackBonus + dieRoll
+    if (dieRoll === 20) {
+      const critDamage = this.damageRoll + this.damageRoll - this.strengthBonus
+      monster.currentHp -= critDamage
+      addGameEvent(`${this._name} critically hit ${monster.name} with their ${this._weapon} for ${damage} damage!!`)
+    }
+    else if (dieRoll === 1) {
+      addGameEvent(`${this._name}'s attack critically missed ${monster.name}!`)
+    }
+    else if (attackRoll >= monster.armorClass) {
       const damage = this.damageRoll
       monster.currentHp -= damage
       addGameEvent(`${this._name} hit ${monster.name} with their ${this._weapon} for ${damage} damage.`)
+    } else {
+      addGameEvent(`${this._name}'s attack missed ${monster.name}.`)
     }
   }
 
@@ -377,3 +388,81 @@ class ActiveCharacter {
       })
   }
 }
+
+class ActiveMonster {
+  constructor(source) {
+    fetch(source, { method: "GET" }).then(resp => resp.json()).then(json => {
+      this._type = json.name
+      this._name = `${fetch('http://faker.hook.io/?property=name.firstName', { method: "GET" }).then(resp => resp.json()).then(name => name)} the ${this._type}`
+
+    })
+  }
+}
+
+{
+  "_id": "5e47666a0b1bb138c516c02b",
+    "index": "goblin",
+      "name": "Goblin",
+        "size": "Small",
+          "type": "humanoid",
+            "subtype": "goblinoid",
+              "alignment": "neutral evil",
+                "armor_class": 15,
+                  "hit_points": 7,
+                    "hit_dice": "2d6",
+                      "speed": { "walk": "30 ft." },
+  "strength": 8,
+    "dexterity": 14,
+      "constitution": 10,
+        "intelligence": 10,
+          "wisdom": 8,
+            "charisma": 8,
+              "proficiencies": [{
+                "name": "Skill: Stealth",
+                "url": "/api/proficiencies/skill-stealth",
+                "value": 6
+              }],
+                "damage_vulnerabilities": [],
+                  "damage_resistances": [],
+                    "damage_immunities": [],
+                      "condition_immunities": [],
+                        "senses": {
+    "darkvision": "60 ft.",
+      "passive_perception": 9
+  },
+  "languages": "Common, 
+  Goblin", 
+  "challenge_rating": 0.25,
+    "special_abilities": [{
+      "name": "Nimble Escape",
+      "desc": "The goblin can take the Disengage or Hide action as a bonus action on each of its turns."
+    }],
+      "actions": [
+        {
+          "name": "Scimitar",
+          "desc": "Melee Weapon Attack: +4 to hit, reach 5 ft., one target. Hit: 5 (1d6 + 2) slashing damage.",
+          "attack_bonus": 4,
+          "damage": [{
+            "damage_type": {
+              "name": "Slashing",
+              "url": "/api/damage-types/slashing"
+            },
+            "damage_dice": "1d6",
+            "damage_bonus": 2
+          }]
+        },
+        {
+          "name": "Shortbow",
+          "desc": "Ranged Weapon Attack: +4 to hit, 
+range 80/320 ft., 
+one target.Hit: 5(1d6 + 2) piercing damage.", 
+"attack_bonus": 4,
+        "damage": [{
+          "damage_type": {
+            "name": "Piercing",
+            "url": "/api/damage-types/piercing"
+          },
+          "damage_dice": "1d6",
+          "damage_bonus": 2
+        }] }],
+"url": "/api/monsters/goblin" }
