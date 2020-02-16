eb1ff4f1cd3a67b1bf37c7740c429788e86f1c30
Jesse <jesse.lee.smith@gmail.com>
2020-02-16 12:18:43 -0800

    fixing errors with updating monsters in db

--- donjons-and-dragoons-api/app/controllers/monsters_controller.rb
+++ donjons-and-dragoons-api/app/controllers/monsters_controller.rb
@@ -1,7 +1,7 @@
 class MonstersController < ApplicationController
   respond_to :json
   def create
-    monster = Monster.new_monster_from_api(params[:monster])
+    monster = Monster.new_monster_from_api(create_monster_params(params))
     if monster.save
       render json: MonsterSerializer.new(monster)
     else
@@ -10,8 +10,22 @@ class MonstersController < ApplicationController
   end
 end
 
+def update
+  monster = Monster.find(params[:id])
+  if monster
+    monster.update(update_monster_params(params))
+    render json: MonsterSerializer.new(monster)
+  else
+    render json: {message: "That monster could not be found!"}
+  end
+end
+
 private
 
 def create_monster_params(params)
   params.require(:monster).permit(:name, :type, :slug, :hit_points, :challenge_rating, :armor_class, :actions[])
+end
+
+def update_monster_params(params)
+  params.require(:monster).permit(:current_hp)
 end
